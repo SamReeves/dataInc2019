08233d6c6f0393d4468a710cd092bbaf8146aa09
TheWerefriend <samtreeves@gmail.com>
2019-05-06 01:06:39 +0630

    Finished scrubbing...

--- clean.R
+++ clean.R
@@ -2,35 +2,41 @@ library(jsonlite)
 library(dplyr)
 library(stringr)
 
+
 # declare some handy vaiables
 setwd("~/Desktop/di/data")
 fileList <- list.files(pattern = "*.json")
 jsonDump <- lapply(fileList, fromJSON)
 dates <- c()
-currencies <- c("USD","VND","THB","SEK","LKR","ZAR","RSD","SAR","RUB",
-		"PHP","PKR","NOK","NZD","MYR","LAK","KWD","KRW","KES",
+dataset = c()
+currencies <- factor(c("USD","VND","THB","SEK","LKR","ZAR","RSD","SAR","RUB",
+		"PHP","PKR","NOK","NZD","NPR","MYR","LAK","KWD","KRW","KES",
 		"ILS","IDR","INR","HKD","EGP","DKK","CZK","CNY","CAD",
 		"KHR","BND","BRL","BDT","AUD","JPY","CHF","GBP","SGD",
-		"EUR")
+		"EUR"))
 
+
+# make an index of dates, convert to Date
 for (i in 1:length(fileList)) {
-# make a list of dataframes and an index of dates
   dates[i] <- substr(fileList[i], 1, 10)
 }
+dates <- as.Date(dates, '%Y.%m.%d')
 
-jsonDump <- lapply(jsonDump, unlist)
 
-
+# Convert to dataframes...
+# Remove useless headings, remove commas
 for (i in 1:length(jsonDump)) {
+	sliver <- unlist(jsonDump[i], use.names=FALSE) %>%
+		tbl_df %>% slice(4:41)
+	sliver <- sapply(sliver, function(v) {
+		as.numeric(gsub("\\,","", as.character(v)))
+	})
 
-# Convert to str, delete headings
-	rawvector[i] <- toString(jsonDump[i]) %>% substring(92)
 
-# Replace NULL values with the last real values
-	if (jsonDump[i] == "") {
-        jsonDump[i] <- jsonDump[i-1]
+# Replace NULL values with previous values
+	if (length(sliver) != length(currencies)) {
+        sliver <- dataset[i-1]
 	}
-	
+
+dataset <- c(dataset, tbl_df(sliver))
 }
-
-
